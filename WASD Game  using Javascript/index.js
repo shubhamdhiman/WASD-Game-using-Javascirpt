cbd113e3bc7ffb8eada8e8b9abe352a43bb9ec23
Shubham <shubhamdhiman00.sd@gmail.com>
2023-06-14 10:05:35 +0530

    Update index.js

--- WASD Game  using Javascript/index.js
+++ WASD Game  using Javascript/index.js
@@ -5,21 +5,21 @@ var fromLeft = 0;
     window.addEventListener("keypress",function(e){
         // console.log(e.keyCode)
         if(e.keyCode == 119){
-            console.log("You pressed W")
+//             console.log("You pressed W")
             if(fromTop>=10){
                 fromTop -= 10;
                 ball.style.top = fromTop + "px"
             }
         }
         else if(e.keyCode == 115){
-            console.log("You pressed S")
+//             console.log("You pressed S")
             if(fromTop<=390){
                 fromTop += 10;
                 ball.style.top = fromTop + "px";
             }
         }
         else if(e.keyCode == 97){
-            console.log("You pressed A")
+//             console.log("You pressed A")
             // ball.style.left = "-10px"
             if(fromLeft>= 10){
                 fromLeft -= 10;
@@ -27,7 +27,7 @@ var fromLeft = 0;
             }
         }
         else if(e.keyCode == 100){
-            console.log("You pressed D")
+//             console.log("You pressed D")
             if(fromLeft<=390){
                 fromLeft += 10;
                 ball.style.left = fromLeft + "px"
